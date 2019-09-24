c7bd7b08d0cd3df96bc4f190ba976011e9e991a7
Rob Martin <robmartin@arista.com>
2019-09-24 11:04:34 -0500

    init

---
+++ pipic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+
+pic_out = "output/"
+file_name = datetime.now().strftime("%Y-%m-%d.%H:%M:%S") + ".jpg"
+
+camera = PiCamera()
+
+camera.start_preview()
+sleep(5)
+camera.capture(pic_out + file_name)
+camera.stop_preview()
+
+print("Done!")
