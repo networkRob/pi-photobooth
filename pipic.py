1bacf120015cb304b25d18aa4b73c0cad1136d4b
Rob Martin <robmartin@arista.com>
2019-09-24 11:13:31 -0500

    cam mods

--- pipic.py
+++ pipic.py
@@ -9,6 +9,10 @@ file_name = datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
 
 camera = PiCamera()
 
+# Cameral configurations:
+camera.resolution = (2592, 1944)
+camera.rotation = 90 
+
 camera.start_preview()
 sleep(5)
 camera.capture(pic_out + file_name)
