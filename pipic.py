98e3583a6498a6a2856067afd8eb2fdd0d545ca2
Rob Martin <robmartin@arista.com>
2019-09-24 11:33:41 -0500

    PIL

--- pipic.py
+++ pipic.py
@@ -3,19 +3,31 @@
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
+from io impoort BytesIO
+from PIL import Image
 
 pic_out = "output/"
 file_name = datetime.now().strftime("%Y%m%d-%H%M%S") + ".jpg"
 
 camera = PiCamera()
+stream = BytesIO()
 
 # Cameral configurations:
 camera.resolution = (3280, 2464)
-camera.rotation = 90 
+# camera.rotation = 90 
 
 camera.start_preview()
 sleep(5)
-camera.capture(pic_out + file_name)
+#camera.capture(pic_out + file_name)
+camera.capture(stream, format="jpeg")
 camera.stop_preview()
+
+stream.seek(0)
+
+image = Image.opent(stream)
+
+image.rotate(90)
+image.save(pic_out + file_name)
+
 
 print("Done!")
