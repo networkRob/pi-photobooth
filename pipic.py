3ce2c774fff14101924b4036a1fc80512924278e
Rob Martin <robmartin@arista.com>
2019-09-24 11:37:34 -0500

    rotate

--- pipic.py
+++ pipic.py
@@ -17,7 +17,7 @@ camera.resolution = (3280, 2464)
 # camera.rotation = 90 
 
 camera.start_preview()
-sleep(5)
+sleep(2)
 #camera.capture(pic_out + file_name)
 camera.capture(stream, format="jpeg")
 camera.stop_preview()
@@ -26,7 +26,7 @@ stream.seek(0)
 
 image = Image.open(stream)
 
-image.rotate(90)
+image = image.rotate(90)
 image.save(pic_out + file_name)
 
 
