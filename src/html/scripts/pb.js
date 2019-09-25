3b54f3caa90d3fbccadbe1de09b715bbc37b145f
Rob Martin <robmartin@arista.com>
2019-09-24 22:01:25 -0500

    using tornado

---
+++ src/html/scripts/pb.js
@@ -0,0 +1,13 @@
+var mURL = 'ws://pb-01:8888/ws-camera';
+var ws = new WebSocket(mURL);
+ws.onopen = function()
+{
+    // Web Socket is connected, send data using send()
+    ws.send("Hello Test Camera");
+    // ws.send(JSON.stringify({type:"Hello",data:"Hello Test"}));
+    // alert("Message is sent...");
+};
+ws.onmessage = function (evt) {
+    var re_data = evt.data;
+    document.getElementById("pres").innerHTML = re_data;
+}
---
+++ src/mgp.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""
+DESCRIPTION
+A Python socket server to act as a backend service for 
+Raspberrypi ZeroW web based photobooth.
+
+"""
+__author__ = 'Rob Martin'
+__version__ = 0.2
+
+# import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+from picamera import PiCamera, PiRenderer
+from time import sleep
+from datetime import datetime, timedelta
+
+l_port = 8888
+
+class cameraRequestHandler(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        print("New connection from: {}".format(self.request.remote_ip))
+        self.write_message("hello user")
+        #self.write_message(json.dumps([lo_sw.data,lo_sw.all_intfs,datetime.now().strftime("%Y-%m-%d %H:%M:%S")]))
+        # self.schedule_update()
+    
+    def on_message(self,message):
+        print("Received: {}".format(message))        
+
+    # def schedule_update(self):
+    #     self.timeout = tornado.ioloop.IOLoop.instance().add_timeout(timedelta(seconds=1),self.update_client)
+    
+    # def update_client(self):
+    #     try:
+    #         #self.write_message(json.dumps([lo_sw.data,lo_sw.all_intfs,datetime.now().strftime("%Y-%m-%d %H:%M:%S")]))
+    #         self.write_message(json.dumps([1,datetime.now().strftime("%Y-%m-%d %H:%M:%S"),lo_sw.data]))
+    #         # sleep(30)
+    #     # except:
+    #     #     print("Connection closed:")
+    #     finally:
+    #         self.schedule_update()
+ 
+    def on_close(self):
+        print('connection closed')
+        tornado.ioloop.IOLoop.instance().remove_timeout(self.timeout)
+ 
+    def check_origin(self, origin):
+        return True
+    
+class mhomeRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('html/index.html')
+
+class boothRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('html/booth.html')
+
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r'/', mhomeRequestHandler),
+        (r'/booth', boothRequestHandler),
+        (r'/ws-camera', cameraRequestHandler)
+    ], static_path="/home/pi/Scripts")
+    app.listen(l_port)
+    print('*** Websocket Server Started on {} ***'.format(l_port))
+    try:
+        tornado.ioloop.IOLoop.instance().start()
+    except KeyboardInterrupt:
+        tornado.ioloop.IOLoop.instance().stop()
+        print("*** Websocked Server Stopped ***")
