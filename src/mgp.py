9489eac7d9bf1b37d743a74fcca126b1c581a854
Rob Martin <robmartin@arista.com>
2019-10-14 19:50:34 -0500

    testing no json

--- src/mgp.py
+++ src/mgp.py
@@ -44,7 +44,8 @@ class cameraRequestHandler(tornado.websocket.WebSocketHandler):
 
     def open(self):
         print("New connection from: {}".format(self.request.remote_ip))
-        self.write_message(json.dumps({'type':'hello','data':"hello user"}))
+        # self.write_message(json.dumps({'type':'hello','data':"hello user"}))
+        self.write_message({'type':'hello','data':"hello user"})
 
     def on_message(self,message):
         print("[{0}] Sent: {1}".format(self.request.remote_ip,message))
