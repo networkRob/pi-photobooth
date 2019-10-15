e2f344db60b06344d6fc94395146b7b705222883
Rob Martin <robmartin@arista.com>
2019-10-15 13:46:00 -0500

    test

--- src/html/scripts/pb.js
+++ src/html/scripts/pb.js
@@ -29,7 +29,7 @@ ws.onmessage = function (evt) {
     // Final photostrip displayed
     else if (received_msg['type'] == 'photo') {
         tmp_output =  "<a href='../booth'>Snap Another?</a>";
-        tmp_output += "<form action='javascript:void(0);'>Print More? <input type='number' value='1' id='pcopy' min='1' max='3'> <input type='button' value='Print More!' onclick='printMore()'></form> "
+        tmp_output += "<form action='javascript:void(0);'>Print More? <input type='number' value='1' id='pcopy' min='1' max='3'> <a onclick='printMore()'>Print More!</a></form> "
         tmp_output += "<br /><br />";
         document.getElementById("imgView").innerHTML =  "<img src='" + received_msg['data'] + "'><br />";
         document.getElementById("countdown").innerHTML = "";
